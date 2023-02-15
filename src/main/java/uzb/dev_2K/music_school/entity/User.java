e6f68ab74ef7d47619573c9eb49489173fae1942
Fathullo.Tuyliyev <asbt2223>
2023-02-15 18:51:15 +0500

    start project 15/02/2023

---
+++ src/main/java/uzb/dev_2K/music_school/entity/User.java
@@ -0,0 +1,9 @@
+package uzb.dev_2K.music_school;
+
+/**
+ * @project music_school
+ * @author: Fathullo To'yliyev on 15/02/2023.
+ * Company: Technologic ASBT
+ */
+public class User {
+}
---
+++ src/test/java/uzb/dev_2K/music_school/MusicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package uzb.dev_2K.music_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
